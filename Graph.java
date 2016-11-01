114f8b73028f8a7ef0513dfa635a7c8ffeafdb8a
PhoebusXS <PhoebusXS@gmail.com>
2016-11-01 23:06:11 +0800

    stylings

--- Graph.java
+++ Graph.java
@@ -3,16 +3,16 @@ public class Graph {
     private final int V;
     private boolean[][] adjMat;
 
-    public Graph (int V) {
+    public Graph(int V) {
         this.adjMat = new boolean[V][V];
         this.V = V;
     }
 
-    public void addEdge (int u, int v) {
+    public void addEdge(int u, int v) {
         this.adjMat[u][v] = true;
     }
 
-    public int V () {
+    public int V() {
         return this.V;
     }
 
